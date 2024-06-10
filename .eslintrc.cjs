15d79fc6eea48b51a23a57726e8ad0f362acdf1b
Prashant Kumar <prashant.aman9@gmail.com>
2024-06-10 16:50:25 +0530

    Add eslint, prettier and cleanup

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    semi: 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/react-in-jsx-scope': 0,
+    'linebreak-style': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+    'comma-dangle': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'react/no-array-index-key': 0,
+  },
+}
